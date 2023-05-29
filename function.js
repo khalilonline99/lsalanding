31f03780a382a56837cde093f40af9ca8acbe282
khalil <khalilonline99@gmail.com>
2023-05-29 18:18:24 +0600

    responsive till our services

--- function.js
+++ function.js
@@ -2,9 +2,15 @@ function openMenu() {
     let navMenu = document.getElementById("nav-menu-top");
     navMenu.classList.add("d-sm-block", "d-xs-block");
     navMenu.classList.remove("d-lg-block","d-xl-block", "d-none");
+    let mobileMenu = document.getElementById("nav-mobile-menu");
+    mobileMenu.classList.remove("d-flex");
+    mobileMenu.classList.add("d-none");
 }
 
 function closeButton() {
-    let navMenu = document.getElementById("nav-menu-top")
-    navMenu.classList.add("d-none", "d-lg-none", "d-xs-none")
+    let navMenu = document.getElementById("nav-menu-top");
+    navMenu.classList.add("d-none", "d-lg-none", "d-xs-none");
+    let mobileMenu = document.getElementById("nav-mobile-menu");
+    mobileMenu.classList.remove("d-none");
+    mobileMenu.classList.add("d-flex");
 }
