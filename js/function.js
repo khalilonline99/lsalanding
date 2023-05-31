eecf89f3bd65b8667804c30c853cb67076daf091
khalil <khalilonline99@gmail.com>
2023-05-31 13:04:39 +0600

    bootstrap menu, removed custom menu

--- js/function.js
+++ js/function.js
@@ -1,9 +1,17 @@
+// function openMenu() {
+//     let navMenu = document.getElementById("nav-menu-top");
+//     navMenu.classList.add("d-sm-block", "d-xs-block");
+//     navMenu.classList.remove("d-lg-block","d-xl-block", "d-none");
+//     let mobileMenu = document.getElementById("nav-mobile-menu");
+//     mobileMenu.classList.remove("d-flex");
+//     mobileMenu.classList.add("d-none");
+// }
 function openMenu() {
     let navMenu = document.getElementById("nav-menu-top");
-    navMenu.classList.add("d-sm-block", "d-xs-block", "wow", "bounceInDown");
-    navMenu.classList.remove("d-lg-block","d-xl-block", "d-none");
+    navMenu.classList.remove("d-none");
+    navMenu.classList.add("d-flex");
     let mobileMenu = document.getElementById("nav-mobile-menu");
-    mobileMenu.classList.remove("d-flex");
+    // mobileMenu.classList.remove("d-flex");
     mobileMenu.classList.add("d-none");
 }
 
