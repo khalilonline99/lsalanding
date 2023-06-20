5b24af7f56a9d7d7fe3c4a8664cc236bed66f0ac
khalil <khalilonline99@gmail.com>
2023-06-20 15:41:19 +0600

    Hero banner, carousels, feedback, courses, changed

--- js/function.js
+++ js/function.js
@@ -1,4 +1,42 @@
 // menu close toggle button
 $(".fa-solid").click(function () {
-    $(".fa-solid").toggleClass("fa-bars fa-xmark fa-lg");
+  $(".fa-solid").toggleClass("fa-bars fa-xmark fa-lg");
 });
+
+
+
+const counters = document.querySelectorAll('.count');
+const speed = 31;
+
+function callback(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Call your function here
+      counters.forEach((counter) => {
+        const updateCount = () => {
+          const target = parseInt(counter.getAttribute('data-target'));
+          const count = parseInt(counter.innerText);
+          const increment = Math.trunc(target / speed);
+
+          if (count < target) {
+            counter.innerText = count + increment;
+            setTimeout(updateCount, 1);
+          } else {
+            counter.innerText = target;
+            //   counter.innerText = count + 1;
+          }
+        };
+        updateCount();
+      });
+    }
+  });
+}
+
+const sectionToObserve = document.querySelector('#counter-section');
+const options = {
+  rootMargin: '0px',
+  threshold: 0.6 // 0.5 means when 50% of the section is visible
+};
+
+const observer = new IntersectionObserver(callback, options);
+observer.observe(sectionToObserve);
